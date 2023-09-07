e6788338ea7cc84f755ff43c7e0ba79f947dd2cd
rstriquer <rstriquer@gmail.com>
2023-09-07 16:21:27 -0300

    feat: Finish Header component

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CTA = () => {
+    return (
+        <div className="cta">
+        <a href="https://docs.google.com/document/d/1ugdBdMMbNJ07XWOFU7dSQ2TgSxUkOmvUVPB5w-NvoIE/export?format=pdf" className="btn">Downalod CV</a>
+            <a href="#contact" className="btn btn-primary">Contact me</a>
+        </div>
+    )
+}
+
+export default CTA
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import CTA from './CTA'
+import HeaderSocials from './HeaderSocials'
+import ME from '../../assets/me.png'
 import './header.css'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <header>
+            <div className="container header__container">
+                <h5>Hi, I am</h5>
+                <h1>Ricardo Soares</h1>
+                <h5 className="text-light">Fullstack Developer and Software Engineer</h5>
+                <CTA />
+                <HeaderSocials />
+
+                <div className="me">
+                    <img src={ME} alt="me" />
+                </div>
+
+                <a href="#contact" className="scroll__down">Scroll Down</a>
+            </div>
+        </header>
     )
 }
 
