9ee61c4b4439fb9fbd2e94868ee7202d4e269a73
rstriquer <rstriquer@gmail.com>
2023-09-08 12:35:02 -0300

    feat: Update component nav to add new modules

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { React, useState }  from 'react'
+import { AiOutlineHome, AiOutlineUser, AiOutlineWhatsApp } from 'react-icons/ai'
+import { TbSchool } from 'react-icons/tb'
+import { MdOutlineDesignServices } from 'react-icons/md'
 import './nav.css'
 
 const Nav = () => {
+    const [activeNav, setActiveNav] = useState('#')
     return (
-        <div>Nav</div>
+        <nav>
+            <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : '' }><AiOutlineHome /></a>
+            <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : '' }><AiOutlineUser /></a>
+            <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : '' }><TbSchool /></a>
+            <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : '' }><MdOutlineDesignServices /></a>
+            <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : '' }><AiOutlineWhatsApp /></a>
+        </nav>
     )
 }
 
