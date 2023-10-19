5cb3600e9e010f446f158ac9ad5f8b63d239a706
rstriquer <rstriquer@gmail.com>
2023-10-19 18:49:40 -0300

    fix: Google Analytics and Cookie div display
    - Change the name of the divs so that they do not have the same name as the component;

--- src/components/cookie_banner/CookieBanner.jsx
+++ src/components/cookie_banner/CookieBanner.jsx
@@ -1,30 +1,30 @@
 import React from 'react'
 import './CookieBanner.css'
 import ReactGA from "react-ga4";
-import { getCookieValue, setCookieValue } from '../../hooks/cookieSetterGetter'
+import { getCookieValue } from '../../hooks/cookieSetterGetter'
 
 const CookieBanner = () => {
     if (getCookieValue('_ga') !== "") {
         // if cookie exists returns without banner.
         // it means banner was accepted previously
         ReactGA.initialize("G-SP6GC1WEK4");
-        return "";
+        return;
     }
 
     const acceptCookies = (event) => {
-        document.querySelector('#cookieBanner').classList.remove('show');
+        document.querySelector('#page-blocker').classList.remove('show');
         ReactGA.initialize("G-SP6GC1WEK4");
     }
 
     return (
-        <div id="cookieBanner" className={getCookieValue('_ga') === "" ? 'alert show' : '' }>
-            <div className="cookie-banner" role="alert">
+        <div id="page-blocker" className={getCookieValue('_ga') === "" ? 'show' : '' }>
+            <div className="message-banner">
                 <p>We use Google Analytics to make your
                     visit more comfortable and <a href=
                     "https://policies.google.com/technologies/cookies?hl=en"
                     target="_blank" rel="noreferrer">Google Analytics uses
                     cookies</a>.<br />We ask that you authorize the use of
-                    cookies in your visits to this site. 
+                    cookies in your visits to this site.
                     <button type="button" class="btn btn-primary acceptcookies"
                     onClick={acceptCookies}>
                         I agree</button></p>
